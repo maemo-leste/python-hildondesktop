e27dd65970707cef3318c694f14248738db9c005
Merlijn Wajer <merlijn@wizzup.org>
2019-12-24 21:06:32 +0100

    move files from trunk to root

--- tags/0.0.3-1maemo2/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Script for running GNU toolchain for configuration
-
-set -x
-libtoolize --automake --copy
-aclocal
-autoconf
-autoheader
-automake --add-missing --foreign --force --copy
